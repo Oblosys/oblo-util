869096067c23eda709e496358ddf212b7bb7df25
Martijn Schrage <martijn@oblomov.com>
2014-10-07 15:49:45 +0200

    Add TypeScript type definitions to package

---
+++ oblo-util.d.ts
@@ -0,0 +1,28 @@
+// Type definitions for oblo-util 0.6.2
+// Definitions by: Martijn Schrage <https://github.com/Oblosys/>
+/// <reference path="../jquery/jquery.d.ts" />
+
+
+interface ObloUtilStatic {
+  debug : boolean;
+  
+  log(...args: any[]) : void;
+  error(...args: any[]) : void;
+  clip(min : number, max : number, x : number) : number;
+  square(x : number) : number;
+  replicate<X>(n : number, x : X) : X[];
+  pad(c : string, l : number, str : any) : string;
+  padZero(l : number, n : number) : string;
+  addslashes(str : string) : string;
+  showJSON(json : any, indentStr? : string, maxDepth? : number) : string;
+  showTime(date : Date) : string;
+  showDate(date : Date) : string;
+  readDate(dateStr : string) : Date;
+  setAttr($elt : JQuery, attrName : string, isSet : boolean) : void;
+}
+
+declare var util: ObloUtilStatic;
+
+declare module "oblo-util" {
+	export = util;
+}
